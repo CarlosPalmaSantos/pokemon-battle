e10868ce6e5d58db4e142dac60b28e0c5815bf6a
CarlosPalmaSantos <carpalsan1@alum.us.es>
2023-02-15 16:15:57 +0100

    a

--- pokemon-battle-cs/pokemon-battle-cs/Program.cs
+++ pokemon-battle-cs/pokemon-battle-cs/Program.cs
@@ -4,7 +4,7 @@ using PokemonBattle.Moves;
 var m = new Move("test", "this is a test", pType.types.GROUND, 100, "targetDamage");
 Move.KeyMovePair.Add("test", m);
 
-Pokemon p1 = new Pokemon("pikachula",0,100,new Pokemon.Stats(100,100,100,100,100,100),pType.types.ELECTRIC,null,"test");
+Pokemon p1 = new Pokemon("pikachulita",0,100,new Pokemon.Stats(100,100,100,100,100,100),pType.types.ELECTRIC,null,"test");
 Pokemon p2 = new Pokemon("pikachurro", 0,100, new Pokemon.Stats(100, 100, 100, 100, 100, 100), pType.types.GROUND, null, "test");
 
 Console.WriteLine(p2.stats.hp);
